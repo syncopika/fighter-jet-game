ed8d38b331f0e9c5bac08a74ee6c583322217115
nicholas hung <nhung93@outlook.com>
2022-07-31 22:50:43 -0400

    added some enemy plane behavior logic

--- Assets/scripts/EnemyController.cs
+++ Assets/scripts/EnemyController.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour
 {
     public GameObject explosion;
-    public Material targetedMaterial;
+    public GameObject target; // the target of the enemy, e.g. the player
+    public Material isTargetedMaterial; // material to show when this model is in attack range of the player
 
     bool isDead = false;
     bool isTargeted = false;
@@ -34,18 +35,39 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        /*
         if (!isDead)
         {
             transform.position += transform.forward * 12f * Time.deltaTime;
-        }
+        }*/
 
         if (isTargeted)
         {
-            transform.GetComponent<MeshRenderer>().material = targetedMaterial;
+            transform.GetComponent<MeshRenderer>().material = isTargetedMaterial;
         }
         else
         {
             transform.GetComponent<MeshRenderer>().material = originalMaterial;
+        }
+
+        if (target && !isDead)
+        {
+            // logic to move to attack target
+
+            // find where the target is
+
+            /* possible conditions to consider? (if the action may be different in each):
+               - if target is above
+               - if target is below
+               - if target is at about the same altitude
+            */
+
+            Vector3 fromTarget = (target.transform.position - transform.position).normalized;
+
+            Quaternion targetRotation = Quaternion.LookRotation(fromTarget);
+
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, 15f * Time.deltaTime);
+            transform.position += transform.forward * 12f * Time.deltaTime;
         }
 
         isTargeted = false;
