9f1f7b1a2311ee142cc823434090a16d14afe9a1
nicholas hung <nhung93@outlook.com>
2022-08-05 23:00:40 -0400

    more progress. worked more on the screen selection scene.

--- Assets/scripts/menu/ChangeSceneButton.cs
+++ Assets/scripts/menu/ChangeSceneButton.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class PlayButton : MonoBehaviour
+public class ChangeSceneButton : MonoBehaviour
 {
     // https://blog.insane.engineer/post/unity_button_load_scene/
     public void LoadScene(string sceneName)
